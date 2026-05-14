83574e178d136ba04efe75eede206a06bb3b3b4e
Adam Davis <adam.davis@dragonfly.com>
2026-05-14 18:53:11 -0500

    sounds

--- src/data/sounds.ts
+++ src/data/sounds.ts
@@ -56,6 +56,61 @@ export const soundList: Sound[] = [
         name: 'Preachy Music',
         path: '/sounds/preachy-music.mp3',
     },
+    {
+        id: 'applause',
+        name: 'Applause',
+        path: '/sounds/applause.mp3',
+    },
+    {
+        id: 'doh',
+        name: 'Doh!',
+        path: '/sounds/doh.mp3',
+    },
+    {
+        id: 'wrong-answer',
+        name: 'Wrong Answer Buzzer',
+        path: '/sounds/wrong-answer.mp3',
+    },
+    {
+        id: 'fart',
+        name: 'Fart',
+        path: '/sounds/fart.mp3',
+    },
+    {
+        id: 'you-are-an-idiot',
+        name: 'You Are An Idiot',
+        path: '/sounds/you-are-an-idiot.mp3',
+    },
+    {
+        id: 'i-find-your-lack-of-faith-disturbing',
+        name: 'I Find Your Lack of Faith Disturbing',
+        path: '/sounds/i-find-your-lack-of-faith-disturbing.mp3',
+    },
+    {
+        id: 'oh-my-god',
+        name: 'Oh My God',
+        path: '/sounds/oh-my-god.mp3',
+    },
+    {
+        id: 'no-god-please-no',
+        name: 'No God Please No',
+        path: '/sounds/no-god-please-no.mp3',
+    },
+    {
+        id: 'who-the-hell-cares',
+        name: 'Who The Hell Cares',
+        path: '/sounds/who-the-hell-cares.mp3',
+    },
+    {
+        id: 'my-god-are-you-still-talking',
+        name: 'My God Are You Still Talking?',
+        path: '/sounds/my-god-are-you-still-talking.mp3',
+    },
+    {
+        id: 'you-are-a-false-prophet',
+        name: 'You Are A False Prophet',
+        path: '/sounds/you-are-a-false-prophet.mp3',
+    },
 ];
 
 /**

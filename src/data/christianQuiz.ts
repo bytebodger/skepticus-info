e9bddca0ea4400ba94fe8b2b318b27dd07bba196
Adam Davis <adam.davis@dragonfly.com>
2026-05-31 18:22:45 -0500

    added exodus questions and configuration options for the quiz

--- src/data/christianQuiz.ts
+++ src/data/christianQuiz.ts
@@ -1364,7 +1364,7 @@ export const christianQuizQuestions: QuizQuestion[] = [
         reference: 'Exodus 33:17',
         answers: [
             'Yahweh knew Moses\'s name, and he knows everyone else\'s name who has ever lived.',
-            'Yahweh knew the names of the prophets, but nowadays, Jesus knows everyone by name.',
+            'Yahweh knew the names of the prophets, but nowadays, only Jesus knows everyone by name.',
             'Yahweh knows your name but he doesn\'t want you to know that he knows your name, otherwise it might go to your head.',
             'Yahweh has known everyone\'s name at some point - but he\'s forgetful.',
             'If it\'s a simple matter of fact that Yahweh knows everyone\'s name, then it\'s nonsensical for him to explicitly tell Moses, "...and I know you by name." The clear implication is that there are a great many people - maybe, nearly all people - who Yahweh does not know by name.',

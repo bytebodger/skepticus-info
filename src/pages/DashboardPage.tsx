bdc94c0695cc84f4452073b430546cca1bd39595
Adam Davis <adam.davis@dragonfly.com>
2026-06-21 12:45:43 -0500

    Updated rules

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -48,10 +48,12 @@ export function DashboardPage()
                             <li>No preaching. Believers are welcome. But if your only "contribution" is to tell everyone that they need Jebus, you can do that shit on Sunday at your local church.</li>
                             <li>Grow a pair. You are under no obligation to call in to talk to the host. But if you feel compelled to constantly bitch about the host in the chat, then grow the hell up and call in for a live conversation.</li>
                             <li>Do not comment on the appearance or speech patterns of any guest. You can make fun of the host, if that makes you feel better about yourself. </li>
+                            <li>No self-promotion in the chat. Although your efforts may be appreciated/supported by other chat participants, there is just no universal rule that can be applied which will logically allow the "right" forms of self-promotion, while weeding out the spammers and scammers.</li>
                         </ol>
                     </li>
                     <li>Callers
                         <ol>
+                            <li>Live conversations are reserved for those who are 18-years-old or older.  This is currently a "soft limit" while we're still only streaming on YouTube.  It will be a much harder limit once we get dual-streaming up-and-running with TikTok, because TikTok has strict limits on callers' minimum ages.</li>
                             <li>More conversation. Less "debate". If you don't know what that means, there are plenty of debate-me-bro channels out there for you to frequent instead of this one.</li>
                             <li>Stop, breathe, and let the other person talk. During animated discussion it's not uncommon for there to be some degree of talking over each other. But if you simply will not stop talking over someone else, don't be surprised if you are muted - or disconnected.</li>
                             <li>The host has the exclusive right to put you on mute at any time. For any reason. If that gets you up in your feels, then Google "how to host my own livestream". And then get the fuck out.</li>
@@ -59,6 +61,8 @@ export function DashboardPage()
                             <li>Although most calls are one-on-one (the host and one caller) there may be other people in the conversation at any point in time. It is not your place to police other guests. If you're bothered by the contribution of another guest, you are more than welcome to start your own livestream. Then you can set all the rules that you want.</li>
                             <li>Refusing to acknowledge when you are demonstrably wrong on basic verifiable facts is a good way to get disconnected. It's also a great way to ensure that we will make highlight videos of your stupidity.</li>
                             <li>Bragging about how you completely destroyed the host, or everyone in the chat, is an excellent way to prove that you're a douchebag and an intellectual toddler.</li>
+                            <li>Believers (of any stripe) are given priority in the call queue. If you're an atheist/agnostic/non, you're welcoem to call in.  But please understand that the conversation is generally more engaging with a distinct difference of opinions.  So please understand if you are skipped over, of if your call is cut short, so we can get to believers who are waiting in the queue.</li>
+                            <li>Self-promotion in live conversations is allowable within the discretion of the host. If you were actively invited to participate, then there's a greater chance that some reasonable self-promotion is acceptable. That chance rises precipitously if you're not being a douchebag during the call.</li>
                         </ol>
                     </li>
                 </ol>
